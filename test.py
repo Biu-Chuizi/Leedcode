d628a9d73d0adacae80fe8387cc74cb0815ca323
John-Joe <guozhe123@outlook.com>
2020-06-30 12:09:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
-def reverse(x):
-    y, res = abs(x), 0
-    boundry = (1<<31) - 1 if x>0 else 1<<31
-    while y != 0:
-        res = res*10 + y%10
-        if res > boundry:
-            return 0
-        y //=10
-    return res if x >0 else -res
-
-
+def make_pizza(*toppings):
+# """概述要制作的比萨"""
+    print("\nMaking a pizza with the following toppings:") 
+    for topping in toppings:
+        print("- " + topping)
 if __name__ == "__main__":
-    reverse(231)
+    make_pizza('pepperoni')
+    make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
